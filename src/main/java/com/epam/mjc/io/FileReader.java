af0e8a30b80b6703da214bf9c576299c41639e4b
Avazbek Isroilov <64166521+Avazbek2002@users.noreply.github.com>
2022-08-26 06:18:18 +0500

    hello

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,60 @@
 package com.epam.mjc.io;
 
 import java.io.File;
-
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file) throws IOException{
+
+        return createProfile(file);
+    }
+
+    private String[] readFile (File file) throws IOException {
+        FileInputStream inputStream = null;
+        String[] finalOutput = null;
+
+        try {
+            inputStream = new FileInputStream(file.getAbsolutePath());
+            finalOutput = new String[4];
+
+            int c, count = 0;
+            while ((c = inputStream.read()) != -1) {
+                if ((char)c == Character.LINE_SEPARATOR) {
+                    count++;
+                    continue;
+                }
+
+                finalOutput[count] = finalOutput[count] + (char)c;
+
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            if (inputStream != null)
+                inputStream.close();
+        }
+
+        return finalOutput;
+    }
+
+    private HashMap<String, String> parseKeyValue (String [] inputString)  {
+        HashMap<String, String> finalOutput = new HashMap<>();
+
+        finalOutput.put("Name", inputString[0].substring(inputString[0].indexOf(':')) + 2);
+        finalOutput.put("Age", inputString[1].substring(inputString[1].indexOf(':')) + 2);
+        finalOutput.put("Email", inputString[2].substring(inputString[2].indexOf(':')) + 2);
+        finalOutput.put("Phone", inputString[3].substring(inputString[3].indexOf(':')) + 2);
+
+        return finalOutput;
+    }
+
+    private Profile createProfile (File file) throws IOException {
+        HashMap<String, String> map = parseKeyValue(readFile(file));
+        return new Profile (map.get("Name"), Integer.parseInt(map.get("Age")),
+                map.get("Email"), Long.parseLong(map.get("Phone")));
     }
 }
